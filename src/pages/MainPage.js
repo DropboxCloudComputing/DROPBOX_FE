7b0df72d0da7a6f3eab620c184e823baf3c3c63a
rlaminseok0824 <xornjsrlaals@gmail.com>
2023-03-30 09:34:31 +0900

    start init

---
+++ src/pages/MainPage.js
@@ -0,0 +1,16 @@
+import {React} from "react";
+import Header from "./common/Header"
+
+const MainPage = () => {
+  //const [uploadOpen, setUploadOpen] = useState(false);
+
+  return(
+    <>
+      <Header/>
+      <h1>Main Page</h1>
+    </>
+    
+  );
+};
+
+export default MainPage;
