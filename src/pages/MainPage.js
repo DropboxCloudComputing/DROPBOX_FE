8ebb64070dad6ec2b888f81304c18a323451cae6
rlaminseok0824 <xornjsrlaals@gmail.com>
2023-03-30 12:17:02 +0900

    ADD: basic header

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,4 +1,4 @@
-import {React} from "react";
+import React from "react";
 import Header from "./common/Header"
 
 const MainPage = () => {
@@ -9,7 +9,6 @@ const MainPage = () => {
       <Header/>
       <h1>Main Page</h1>
     </>
-    
   );
 };
 
--- src/pages/common/Header.js
+++ src/pages/common/Header.js
@@ -1,13 +1,27 @@
-
+import React from "react";
 import { Navbar,Container } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../styles/Header.css'
+import Logo from "../../logo.svg"
 
 const Header = () => {
   return(
-    <Navbar expand="lg" variant="dark" bg="dark">
-      <Container>
-        <Navbar.Brand href="/">Navbar</Navbar.Brand>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#">
+            <img
+              alt=""
+              src= {Logo}  //put our logo image here
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            DROPBOX
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
