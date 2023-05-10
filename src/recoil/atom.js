aad3d0ed24f917d7457036ff9e0ba1e351533fdf
rlaminseok0824 <xornjsrlaals@gmail.com>
2023-05-10 14:16:55 +0900

    FIX: sharedFolder atom

--- src/recoil/atom.js
+++ src/recoil/atom.js
@@ -7,5 +7,15 @@ export const fileListState = atom({
         { id: 3, name: 'file3.docx', size: 987654 },
         { id: 4, name: 'file3.docx', size: 987654 },
         { id: 5, name: 'file3.docx', size: 987654 },
-        { id: 6, name: 'file3.docx', size: 987654 },]
+        { id: 6, name: 'file3.docx', size: 987654 },
+        ]
+});
+
+
+export const sharedFolderState = atom({
+    key : 'sharedList',
+    default : [
+        { id: 1, name: '클라우드 컴퓨팅'},
+        { id: 2, name: '소프트웨어 공학'},
+    ]
 });
