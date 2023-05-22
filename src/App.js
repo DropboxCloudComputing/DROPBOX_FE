639e7b60b67bda3aed2a15f9ed08701767b35dbf
aon97 <aon97@aon97s-MacBook-Pro.local>
2023-05-22 15:40:15 +0900

    Adding Register Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import React from "react";
 import {Routes,Route} from "react-router-dom";
 import Login from "./pages/LoginPage";
+import Register from "./pages/RegisterPage";
 import MainPage from "./pages/MainPage";
 import TrashbinPage from "./pages/TrashbinPage";
 
@@ -13,6 +14,7 @@ function App() {
         <Route element={<MainPage />} path="/" />
         <Route element={<TrashbinPage />} path="/trashbin"/>
         <Route element={<Login />} path="/login" />
+        <Route element={<Register />} path="/register" />
       </Routes>
     </>
   );
---
+++ src/containers/RegisterContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import RegisterComponent from "../components/RegisterComponent";
+
+const RegisterContainer = () => {
+    return (<RegisterComponent />);
+};
+
+export default RegisterContainer;
